cf37a430d378f4818941c500e617e92f9d319172
Heather Moreira <moreiraheather@gmail.com>
2020-04-09 23:49:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,7 @@ class Artist
   end 
   
   def new_song(name, genre)
-    artist_name = Atrist.new(name)
+    artist_name = Artist.new(name)
     artist_name.genre = genre 
     artist_name.artist = self 
   end 
