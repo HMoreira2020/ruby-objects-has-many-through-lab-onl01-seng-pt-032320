e591c2d719cb501760c6efb7bf49f6c7034a6d68
Heather Moreira <moreiraheather@gmail.com>
2020-04-09 23:47:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,32 @@
 class Song 
+  attr_accessor :name, :genre  
   
+  @@all = [] 
+  
+  def initialize(name, artist, genre) 
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(name, genre)
+    artist_name = Atrist.new(name)
+    artist_name.genre = genre 
+  end 
+  
+  def songs 
+    Song.all.select{|song| song.artist == self} 
+  end 
+  
+  def genres 
+    self.songs.collect{|song| song.genre} 
+  end 
+
   
   
   
